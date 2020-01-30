d1a54771c1e5c236af1771fe6ad660bf48c9b367
Ryusuke-1975 <masked1971kick@gmail.com>
2020-01-30 22:11:25 +0000

    課題2です

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -7,13 +7,12 @@ console .log(firstName + familyName)
 
 console .log(1+2+3+4+5+6+7+8+9+10)
 
-const dog = true
+
+
+const dog = false
 const cat = true
 
-console .log(typeof dog)
-console .log(typeof cat)
-
-console .log(dog||cat)
-console .log(dog&&cat)
+console .log(dog || cat)
+console .log(dog && cat)
 
 console .log(!dog && !cat)
